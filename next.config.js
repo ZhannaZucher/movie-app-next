e25453d3d1e38cd8ad69b72d73aea607892dc509
ZhannaZucher <jzucher@gmail.com>
2023-09-28 09:36:49 +0200

    add fontawesome

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/original/**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
