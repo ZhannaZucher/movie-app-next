fea93faa57c01ef0b445e34e3894ca463b8b0aa5
ZhannaZucher <jzucher@gmail.com>
2023-09-28 19:22:21 +0200

    add form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -2,11 +2,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons"
 import styles from "../styles/SearchForm.module.scss"
+import { setSearchQuery } from "@/app/store/moviesSlice"
+import { useState } from "react"
+import { useAppDispatch } from "@/app/store/selectors"
 
 const SearchForm = () => {
+  const [query, setQuery] = useState("")
+  const dispatch = useAppDispatch()
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    dispatch(setSearchQuery(query))
+  }
+
   return (
-    <form className={styles.form}>
-      <input type="text" placeholder="Entrez le titre" />
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Entrez le titre"
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <button type="submit">Rechercher</button>
       <div>
         <button>
